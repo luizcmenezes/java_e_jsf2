540756a183f296a100fb8aae2b70bb85e038854c
luizcmenezes <lcmscontas@gmail.com>
2020-12-12 15:10:12 -0300

    Introdução ao primefaces

---
+++ livraria-orig/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,41 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.caelum.livraria.dao.UsuarioDao;
+import br.com.caelum.livraria.modelo.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public String login() {
+		System.out.println("Teste login" + this.usuario.getEmail());
+		FacesContext context = FacesContext.getCurrentInstance();
+		boolean existe = new UsuarioDao().existe(this.usuario);
+		if (existe) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return "livro?faces-redirect=true";
+		}
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário ou senha não existe!"));
+		
+		return "login?faces-redirect=true";
+	}
+	
+	public String sair() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
+	}
+}
